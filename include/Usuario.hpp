f97b27c96aebad80d23f95641730f80e0034ca41
victorcabral1 <147670536+victorcabral1@users.noreply.github.com>
2023-11-17 22:37:59 -0300

    Update Usuario.hpp
    definindo atributos e métodos da classe Usuario

--- include/Usuario.hpp
+++ include/Usuario.hpp
@@ -1,12 +1,15 @@
 //TODO - Incluir colaborações
+#pragma once
+#include <string>
 
 class Usuario {
 public:
-
-    // TODO - Incluir métodos de usuário
+// TODO - Incluir métodos de usuário
+Usuario(const std::string &nome, const std::string senha, const std::string tipoUsuario);
 
 private:
-
-    // TODO - Incluir atributos de usuário
-
+// TODO - Incluir atributos de usuário
+const std::string nome;
+const std::string senha;
+const std::string tipoUsuario;
 };
