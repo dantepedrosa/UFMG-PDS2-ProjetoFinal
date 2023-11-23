e28c06be69aae81eea987024d43b74f0ec30822e
victorcabral1 <147670536+victorcabral1@users.noreply.github.com>
2023-11-23 17:45:34 -0300

    Update BancoDeDados.hpp

--- include/BancoDeDados.hpp
+++ include/BancoDeDados.hpp
@@ -1,12 +1,19 @@
 //TODO - Incluir colaborações
+#pragma once
+#include <iostream>
+#include <map>
+#include <vector>
+#include "Livro.hpp"
+
 
 class BancoDeDados {
 public:
-
     // TODO - Incluir métodos de usuário
+    void adicionarLivros(Livro &livro);
+    void mostrarTodosLivros();
+    std::vector<Livro> obterLivrosPorEstado(std::string _estado);
 
 private:
-
     // TODO - Incluir atributos de usuário
-
+std::map<std::string, Livro> _livros;
 };
