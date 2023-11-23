39603da81c1925105596522d660d99654a093d10
victorcabral1 <147670536+victorcabral1@users.noreply.github.com>
2023-11-23 17:23:51 -0300

    Update Administracao.hpp

--- include/Administracao.hpp
+++ include/Administracao.hpp
@@ -1,8 +1,12 @@
 //TODO - Incluir colaborações
+#pragma once
+#include <iostream>
+#include "BancoDeDados.hpp"
 
 class Administracao {
 public:
-
+    static void adicionarNovoLivro(BancoDeDados &banco);
+    static void removerLivro(BancoDeDados &banco);
     // TODO - Incluir métodos de usuário
 
 private:
