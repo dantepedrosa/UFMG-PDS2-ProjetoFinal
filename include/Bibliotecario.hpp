c0f6502d859c29e8615327fc621f773952a61435
victorcabral1 <147670536+victorcabral1@users.noreply.github.com>
2023-11-25 21:16:29 -0300

    Update Bibliotecario.hpp

--- include/Bibliotecario.hpp
+++ include/Bibliotecario.hpp
@@ -1,12 +1,14 @@
 //TODO - Incluir colaborações
-
+#pragma once
+#include <string>
 class Bibliotecario {
 public:
-
-    // TODO - Incluir métodos de usuário
-
+// TODO - Incluir métodos de usuário
+    void receberAlertaLivroAtrasado(BancoDeDados &banco, Locacao& locacao);
+    void alterarEstadoLivro(BancoDeDados &banco, std::string &_nomeLivro, std::string &novoEstado);
+    void mostrarLivrosAtrasados(BancoDeDados& banco) const;
+    void mostrarUsuariosComLivrosAtrasados(BancoDeDados& banco) const;
 private:
-
-    // TODO - Incluir atributos de usuário
+// TODO - Incluir atributos de usuário
 
 };
