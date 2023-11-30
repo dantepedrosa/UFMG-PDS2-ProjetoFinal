73d5625784fe68b431e78d83e64a02bde78b5966
Dante Pedrosa <dantepedrosa@gmail.com>
2023-11-29 21:59:24 -0300

    Organizado Usuario

--- include/Usuario.hpp
+++ include/Usuario.hpp
@@ -7,7 +7,6 @@ using namespace std;
 
 class Usuario {
 public:
-    // TODO - Incluir métodos de usuário
     
     /// @brief Construtor da classe Usuário
     /// @param nome nome de usuário (aceito apenas nomes sem espaço)
@@ -15,14 +14,8 @@ public:
     /// @param tipoUsuario Tipo do usuário: Usuário comum, adm e bibliotecário 
     Usuario(string nome, int senha, string tipoUsuario);
     
-    
-
-    // TODO - Mudar implementação usuário e administração
 private:
-    // TODO - Incluir atributos de usuário
     
-    /// @brief Nome do usuário
     string _nome;
     int _senha;
-    string _tipoUsuario;
 };
