daa97a0ec71c3ec0c9ebdd58e86580c1e24b56df
Dante Pedrosa <dantepedrosa@gmail.com>
2023-11-30 09:07:16 -0300

    Fixed - Include errors

--- test/testAdministracao.cpp
+++ test/testAdministracao.cpp
@@ -1,7 +1,7 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #define DOCTEST_CONFIG_SUPER_FAST_ASSERTS
 
-#include "lib\doctest.hpp"
+#include "lib/doctest.hpp"
 
 TEST_CASE(""){
     // TODO - Test case
---
+++ test/testSistemaLivros.cpp
@@ -0,0 +1,8 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#define DOCTEST_CONFIG_SUPER_FAST_ASSERTS
+
+#include "lib/doctest.hpp"
+
+TEST_CASE(""){
+    // TODO - Test case
+}
