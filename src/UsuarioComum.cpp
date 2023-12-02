a160dc516e63f681938e8c9e8d113647774f731b
SAlex758 <147111426+SAlex758@users.noreply.github.com>
2023-12-02 15:21:52 -0300

    Update UsuarioComum.cpp

--- src/UsuarioComum.cpp
+++ src/UsuarioComum.cpp
@@ -1,18 +1,25 @@
 #include <cctype>
 #include <iostream>
+#include <stdexcept>
 
 #include "UsuarioComum.hpp"
 
-class ExcecaoDeNomeInvalido {}
 class ExcecaoLivroJaEmprestado{}
 
 UsuarioComum::UsuarioComum(std::string nome, int senha){
+    //verificador, se contém algo diferente de número e letra na string recebe true
+    bool verificador = false;
+
     for (auto c : nome) {
-        if (!isalpha(c)) {
-            std::cout << "Nome inválido! Utilize apenas letras." << std::endl;
-            throw ExcecaoDeNomeInvalido();
+        if (!isalpha(c) && !isdigit(c)) {
+            verificador = true;
+            break;
         }
     }
+
+    if (verificador) 
+    throw std::invalid_argument("Nome inválido! Utilize apenas números e letras.");
+    
     this->nome = nome;
     this->senha = senha;
 };
@@ -24,20 +31,19 @@ bool UsuarioComum::solicitarEmprestimo(CodISBN isbn){
     std::cout << "Usuário com pendência. Não é possível realizar empréstimo." << std::endl;
     return false;
   }
-    if (_livrosEmprestados.find(isbn) != _livrosEmprestados.end()) {
-    std::cout << "Livro já emprestado pelo usuário." << std::endl;
-    throw ExcecaoLivroJaEmprestado();
-  }
+    if (_livrosEmprestados.find(isbn) != _livrosEmprestados.end()) 
+    throw std::invalid_argument("Livro já emprestado pelo usuário.");
+    
     return true;
 };
 
 
 bool UsuarioComum::devolverLivro(CodISBN isbn){
-    auto it = _livrosEmprestados.find(isbn);
-    if (it == _livrosEmprestados.end()) {
-        std::cout << "O livro não foi emprestado por este usuário." << std::endl;
-        return false;
-    }
+auto it = _livrosEmprestados.find(isbn);
+    
+    if (it == _livrosEmprestados.end())
+    throw std::invalid_argument("O livro não foi emprestado por este usuário.");
+    
     return true;
 }
 
@@ -59,4 +65,14 @@ bool UsuarioComum::pagarGRU(std::string gru){
 
     std::cout << "Pagamento da GRU realizado com sucesso." << std::endl;
     return true;
+}
+bool UsuarioComum::getPendencia() const {
+    return _comPendencia;
+}
+
+bool UsuarioComum::temLivroEmprestado() const{
+    if(!_livrosEmprestados.empty()){
+        return true;
+    }
+    return false;
 }
