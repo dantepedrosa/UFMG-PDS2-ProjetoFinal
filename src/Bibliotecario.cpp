1e532d3b8dce984481fe657e0f6c27040b5da9f2
SAlex758 <147111426+SAlex758@users.noreply.github.com>
2023-12-02 15:54:39 -0300

    Update Bibliotecario.cpp

--- src/Bibliotecario.cpp
+++ src/Bibliotecario.cpp
@@ -1,8 +1,18 @@
 #include "Bibliotecario.hpp"
-
+#include <stdexcept>
 
 bool Bibliotecario::liberarEmprestimo(Usuario &u, CodISBN isbn){
     // TODO - Implementar função
+    if(u.getPendencia()){
+        throw std::invalid_argument("Usuário com pendência, não é possível liberar o empréstimo.");
+    }
+    
+    if(u.temLivroEmprestado()){
+        throw std::invalid_argument("Usuário já tem livro emprestado, não é possível liberar o empréstimo.");
+    }
+    
+    std::cout << "Empréstimo realizado com sucesso." << std::endl;
+    return true;
 }
 
 
