5da8e648b305a7a629b09c5969f98a9527618203
Rod <142761357+RodSFarias@users.noreply.github.com>
2023-12-02 12:06:54 -0300

    Update UsuarioComum.cpp

--- src/UsuarioComum.cpp
+++ src/UsuarioComum.cpp
@@ -1,22 +1,62 @@
+#include <cctype>
+#include <iostream>
+
 #include "UsuarioComum.hpp"
 
+class ExcecaoDeNomeInvalido {}
+class ExcecaoLivroJaEmprestado{}
 
 UsuarioComum::UsuarioComum(std::string nome, int senha){
-    // TODO - Implementar função
-}
+    for (auto c : nome) {
+        if (!isalpha(c)) {
+            std::cout << "Nome inválido! Utilize apenas letras." << std::endl;
+            throw ExcecaoDeNomeInvalido();
+        }
+    }
+    this->nome = nome;
+    this->senha = senha;
+};
 
 
 
 bool UsuarioComum::solicitarEmprestimo(CodISBN isbn){
-    // TODO - Implementar função
-}
+    if (_comPendencia) {
+    std::cout << "Usuário com pendência. Não é possível realizar empréstimo." << std::endl;
+    return false;
+  }
+    if (_livrosEmprestados.find(isbn) != _livrosEmprestados.end()) {
+    std::cout << "Livro já emprestado pelo usuário." << std::endl;
+    throw ExcecaoLivroJaEmprestado();
+  }
+    return true;
+};
 
 
 bool UsuarioComum::devolverLivro(CodISBN isbn){
-    // TODO - Implementar função
+    auto it = _livrosEmprestados.find(isbn);
+    if (it == _livrosEmprestados.end()) {
+        std::cout << "O livro não foi emprestado por este usuário." << std::endl;
+        return false;
+    }
+    return true;
 }
 
 
 bool UsuarioComum::pagarGRU(std::string gru){
-    // TODO - Implementar função
+    if (!_comPendencia) {
+        std::cout << "Usuário não possui pendência para pagar." << std::endl;
+        return false;
+    }
+
+    auto it = _grusAPagar.find(gru);
+    if (it == _grusAPagar.end()) {
+        std::cout << "GRU não encontrada na lista de pendências." << std::endl;
+        return false;
+    }
+
+    _grusAPagar.erase(it);
+    _comPendencia = _grusAPagar.size() > 0;
+
+    std::cout << "Pagamento da GRU realizado com sucesso." << std::endl;
+    return true;
 }
