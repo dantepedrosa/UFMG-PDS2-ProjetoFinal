8808a2a6d3dd930233ba0b4bfa2e92a47983d944
victorcabral1 <147670536+victorcabral1@users.noreply.github.com>
2023-12-04 19:54:27 -0300

    Update testAdministracao.cpp

--- test/testAdministracao.cpp
+++ test/testAdministracao.cpp
@@ -3,6 +3,34 @@
 
 #include "lib/doctest.hpp"
 
-TEST_CASE(""){
-    // TODO - Test case
+#include "Administracao.hpp"
+
+TEST_CASE("Adicionar Livro") {
+    // TODO: instancia de adm
+    Administracao administracao;
+    Livro livro("Livro Teste", "Autor Teste", 2023);
+    bool resultado = administracao.adicionarLivro(livro);
+
+    CHECK(resultado == true);
+}
+
+TEST_CASE("Remover Livro") {
+
+    Administracao administracao;
+    Livro livro("Livro Teste", "Autor Teste", 2023);
+    administracao.adicionarLivro(livro);
+    
+    bool resultado = administracao.removerLivro(livro);
+
+    CHECK(resultado == true);
+}
+
+TEST_CASE("Remover Livro Inexistente") {
+
+    Administracao administracao;
+    Livro livroInexistente("Livro Inexistente", "Autor Inexistente", 2023);
+
+    bool resultado = administracao.removerLivro(livroInexistente);
+
+    CHECK(resultado == false);
 }
