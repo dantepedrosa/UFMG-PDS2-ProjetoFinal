e50fe2c31fbd2a248f59cad1107de28aaaab66dc
SAlex758 <147111426+SAlex758@users.noreply.github.com>
2023-12-04 23:05:42 -0300

    Update SistemaLivros.cpp

--- src/SistemaLivros.cpp
+++ src/SistemaLivros.cpp
@@ -1,39 +1,50 @@
 #include "SistemaLivros.hpp"
 
 
-bool SistemaLivros::adcionarLivro(Livro livro){
+SistemaLivros::SistemaLivros(bool readFirst, std::string path){
+    BancoDeDados(readFirst, path, *this);
+    if(readFirst){
+        bd.lerBancoDeDados(*this);
+    }
+}
+
+bool SistemaLivros::adicionarLivro(Livro livro){
     
-	if (_biblioteca.find(livro.getISBN()) != _biblioteca.end()) {
-        std::cout << "Livro já existente na biblioteca." << std::endl;
-        return false;
+    std::string isbn = livro.getISBN();
+
+    // Se existe um livro soma o numero de copias
+    auto find = biblioteca.find(isbn);
+	if (find != biblioteca.end()) {
+        find->second.copias++;
+        return true;
     }
 
-    _biblioteca[livro.getISBN()] = {
-        livro.getNome(),
-        livro.getAutor(),
-        livro.getAssunto(),
-        livro.getAno()
-    };
+    // Salva no acervo
+    LivroDB dbLivro;
+    
+    dbLivro.nome = livro.getNome();
+    dbLivro.autor = livro.getAutor();
+    dbLivro.assunto = livro.getAutor();
+    dbLivro.anoPublicacao = std::to_string(livro.getAno());
+    dbLivro.copias = 0;
 
-    _byAno[livro.getAno()].insert(livro.getISBN());
-    _byAssunto[livro.getAssunto()].insert(livro.getISBN());
-    _byAutor[livro.getAutor()].insert(livro.getISBN());
+    biblioteca[isbn] = dbLivro;
 
-    bd.salvarAcervo(*this);
+    // Salva nas listas de categorias
+    byAno[dbLivro.anoPublicacao].insert(isbn);
+    byAssunto[dbLivro.assunto].insert(isbn);
+    byAutor[dbLivro.autor].insert(isbn);
 
-    std::cout << "Livro adicionado com sucesso." << std::endl;
+    this->salvarListas();
+
     return true;
 }
 
 
-bool SistemaLivros::adcionarLivro(CodISBN isbn){
-    // TODO - Implementar função
-}
-
-
 bool SistemaLivros::editarLivro(CodISBN isbn, LivroDB livro){
-    auto it = _biblioteca.find(isbn);
-    if (it == _biblioteca.end()) {
+
+    auto it = biblioteca.find(isbn);
+    if (it == biblioteca.end()) {
         std::cout << "Livro não encontrado na biblioteca." << std::endl;
         return false;
     }
@@ -46,9 +57,9 @@ bool SistemaLivros::editarLivro(CodISBN isbn, LivroDB livro){
     it->second.copias = livro.copias;
 
     // atualizando nas listas
-    _byAno[livro.anoPublicacao].insert(isbn);
-    _byAssunto[livro.assunto].insert(isbn);
-    _byAutor[livro.autor].insert(isbn);
+    byAno[livro.anoPublicacao].insert(isbn);
+    byAssunto[livro.assunto].insert(isbn);
+    byAutor[livro.autor].insert(isbn);
 
     std::cout << "Livro editado com sucesso." << std::endl;
     return true;
@@ -58,20 +69,20 @@ bool SistemaLivros::editarLivro(CodISBN isbn, LivroDB livro){
 bool SistemaLivros::removerLivro(Livro livro){
 
     CodISBN isbn = livro.getISBN();
-    auto it = _biblioteca.find(isbn);
-    if (it == _biblioteca.end()) {
+    auto it = biblioteca.find(isbn);
+    if (it == biblioteca.end()) {
         std::cout << "Livro não encontrado na biblioteca." << std::endl;
         return false;
     }
 
-    unsigned ano = livro.getAno();
+    std::string ano = std::to_string(livro.getAno());
     std::string assunto = livro.getAssunto();
     std::string autor = livro.getAutor();
 
-    _biblioteca.erase(it);
-    _byAno[ano].erase(isbn);
-    _byAssunto[assunto].erase(isbn);
-    _byAutor[autor].erase(isbn);
+    biblioteca.erase(it);
+    byAno[ano].erase(isbn);
+    byAssunto[assunto].erase(isbn);
+    byAutor[autor].erase(isbn);
 
     std::cout << "Livro removido com sucesso." << std::endl;
     return true;
@@ -79,32 +90,28 @@ bool SistemaLivros::removerLivro(Livro livro){
 
 
 bool SistemaLivros::salvarListas(){
-    // TODO - Implementar função
+    bd.salvarAcervo(*this);
+    bd.salvarCategorias(*this);
 }
 
 
 bool SistemaLivros::emprestarLivro(CodISBN isbn, UsuarioComum &u){
 
-    auto it = _biblioteca.find(isbn);
-    if (it == _biblioteca.end()) {
+    auto it = biblioteca.find(isbn);
+    if (it == biblioteca.end()) {
         std::cerr << "Livro não encontrado na biblioteca." << std::endl;
         return false;
     }
 
-	if(temLivroEmprestado){
-		std::cerr << "O usuário já possui um livro emprestado." << std::endl;
+	if(u.livrosEmprestados.size() >= 5){
+		std::cerr << "O usuário já alcançou o limite de livros emprestados" << std::endl;
 		return false;
 	}
 
 
-    _livrosEmprestados.push_back(std::make_pair(isbn, u.getNome()));
+    livrosEmprestados.push_back(std::make_pair(isbn, u.getNome()));
 
     std::cout << "Livro emprestado com sucesso." << std::endl;
     return true;
 
-}
-
-
-bool SistemaLivros::devolverLivro(CodISBN isbn){
-    // TODO - Implementar função
 }
