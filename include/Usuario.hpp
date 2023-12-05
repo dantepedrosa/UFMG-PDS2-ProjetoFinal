2d1dc7b2527bc11d2aebc52c32ce55f109f06159
SAlex758 <147111426+SAlex758@users.noreply.github.com>
2023-12-04 22:12:15 -0300

    Update Usuario.hpp

--- include/Usuario.hpp
+++ include/Usuario.hpp
@@ -1,19 +1,25 @@
 #pragma once
 
 #include <string>
+#include <stdexcept>
 
-
+/// @brief Classe base de usuários do sistema de biblioteca
 class Usuario {
 public:
     
     /// @brief Construtor da classe Usuário
-    /// @param nome nome de usuário (aceito apenas nomes sem espaço)
+    /// @param nome Nome de usuário (aceito apenas letras sem espaço). Max. 20 characteres
     /// @param senha senha do usuário (apenas números)
-    /// @param tipoUsuario Tipo do usuário: Usuário comum, adm e bibliotecário 
     Usuario(std::string nome, int senha);
     
 private:
+
+    /// @brief Setter do nome
+    /// @details Gera exceção caso nome seja maior que 20 caracteres ou possua character diferente de letras.
+    /// @param nome Nome de usuário
+    void _setNome(std::string nome);
+
     
-    std::string _nome;
-    int _senha;
+    std::string _nome;  ///< Nome do usuário (apenas letras)
+    int _senha;         ///< Senha do usuário
 };
