0c6a77e32e726a949721c2f67442010bf04ef8ac
Pedrojoy <148808392+Pedrojoy@users.noreply.github.com>
2023-12-04 23:09:58 -0300

    Update Administracao.hpp

--- include/Administracao.hpp
+++ include/Administracao.hpp
@@ -1,27 +1,33 @@
 #pragma once
 
 #include "Usuario.hpp"
-#include <iostream>
 #include "Livro.hpp"
 #include "SistemaLivros.hpp"
 
 class Administracao : Usuario{
 public:
 
+    /// @brief Construtor da classe de usuários da administração do sistema de bibliotecas 
+    /// @param nome Nome de usuário (apenas letras) - Max 20 char
+    /// @param senha Senha do usuário (apenas números)
+    Administracao(std::string nome, int senha);
+
     /// @brief Adciona livro no banco de dados e do sistema de livros
     /// @details Esta função apenas chama a função de mesmo nome da classe SistemaLivros.
     /// @param livro Instância de Livro
     /// @return True se foi adicionado com sucesso e False caso haja falha
-    bool adcionarLivro(Livro livro);
+    void adcionarLivro(Livro livro);
 
 
     /// @brief Remove livro do bando de dados e do sistema de livros
     /// @details Esta função apenas chama a função de mesmo nome da classe SistemaLivros.
     /// @param livro Instância de Livro
     /// @return True se foi removido com sucesso e False caso haja falha
-    bool removerLivro(Livro livro);
+    void removerLivro(Livro livro);
 
 
 private:
+
+    SistemaLivros _sl;  ///< Intância de sistema livros.
 
 };
