8e6989fcad87e5b2b67d20764a3d84cfcf517b56
SAlex758 <147111426+SAlex758@users.noreply.github.com>
2023-12-04 22:12:33 -0300

    Update Usuario.cpp

--- src/Usuario.cpp
+++ src/Usuario.cpp
@@ -1,6 +1,19 @@
 #include "Usuario.hpp"
 
 
-Usuario::Usuario(std::string nome, int senha){
-    // TODO - Implementar função
+Usuario::Usuario(std::string nome, int senha) : _senha(senha){
+    _setNome(nome);
+}
+
+void _setNome(std::string nome){
+
+    // Confere o tamanho do nome
+    if(nome.length() > 20) 
+        throw std::invalid_argument("Nome de usuário muito longo!");
+        
+    // Confere se possui apenas letras
+    for(auto letra : nome){
+        if(!((letra >= 'A' && letra <= 'Z') || (letra >= 'a' && letra <= 'z'))) 
+            throw std::invalid_argument("O nome deve possuir apenas letras.");
+    }
 }
